38f88b8d54b1253acf95a0a3c4fc22d4f7bf3d3b
Junho Oh <junho5@vt.edu>
2024-09-20 12:46:49 +0900

    Cleaned up the setup script.

--- setup.py
+++ setup.py
@@ -11,10 +11,8 @@ def experiment_CycleGAN(data_dir, save_dir,
                         dataset, batch_size=1,
                         epochs=100, groups=1,
                         saved_location=None, trained_epoch=0,
-                        retrain=False, root_dir=None, old_model=False, dataType='Portal'):
-    if root_dir == None:
-        print("No root directory")
-        return
+                        old_model=False, dataType='Portal'):
+    
     input_dir = os.path.join(root_dir, data_dir)
 
     train_loader, test_loader = util.initialize_datasets(input_dir, batch_size, percentage=0.8)
@@ -30,22 +28,8 @@ def experiment_CycleGAN(data_dir, save_dir,
     if saved_location:
         trained_dir = os.path.join(new_dir, saved_location)
         trained_dir = os.path.join(trained_dir, 'checkpoints')
-        if old_model:
-            generator_A2B.load_state_dict(
-                torch.load(os.path.join(trained_dir, f"generator_A2B_epoch_{trained_epoch}.pth")), strict=False)
-        else:
-            generator_A2B.load_state_dict(torch.load(os.path.join(trained_dir, f"generator_A2B_epoch_{trained_epoch}.pth")))
-        if retrain == True:
-            train_loader, test_loader = util.initialize_datasets(input_dir, dataset, batch_size)
-            generator_A2B, generator_B2A = train.train_cycle_gan(train_loader,
-                                                       generator_A2B,
-                                                       generator_B2A,
-                                                       discriminator_A,
-                                                       discriminator_B,
-                                                       num_epochs=epochs,
-                                                       prevEpoch=trained_epoch, root_dir=out_dir)
+        generator_A2B.load_state_dict(torch.load(os.path.join(trained_dir, f"generator_A2B_epoch_{trained_epoch}.pth")), strict=False)
     else:
-        # train_loader, test_loader = initialize_datasets(input_dir, dataset, batch_size)
         generator_A2B, generator_B2A = train.train_cycle_gan(train_loader,
                                                generator_A2B,
                                                generator_B2A,
